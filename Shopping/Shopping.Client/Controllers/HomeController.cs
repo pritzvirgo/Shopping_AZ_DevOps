302d3df46702ab73e9f179a1873174404cbe7f8a
pmehta-sunwell <161654958+smehta-sunwell@users.noreply.github.com>
2024-08-22 09:32:34 -0500

    Update HomeController.cs
    Product Mock data added.

--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using Shopping.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO.Pipelines;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Shopping.Client.Controllers
 {
@@ -31,11 +33,26 @@ namespace Shopping.Client.Controllers
 
         public async Task<IActionResult> Products()
         {
-            var response = await _httpClient.GetAsync("/api/product");
-            var content = await response.Content.ReadAsStringAsync();
-            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
-
-            return View("Products",productList);
+            try
+            {
+                var response = await _httpClient.GetAsync("/api/product");
+                var content = await response.Content.ReadAsStringAsync();
+                var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+                return View("Products", productList);
+            }
+            catch (Exception ex)
+            {
+                var productList = new List<Product>() { new Product() {
+                    Name = "Demo Product",
+                    Description = "This is Demo Product, loresum description.",
+                    ImageFile = "product-1.png",
+                    Price = 1500.00M,
+                    Category = "Alibaba Xing"
+                }
+                };
+                return View("Products", productList);
+            }
+           
         }
 
         public IActionResult Privacy()
